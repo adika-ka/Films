d68f1d9e58cb6758132d9e7a8ff6513abf0cc2f0
adika-ka <adika-ka@mail.ru>
2022-06-05 18:14:35 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const numberOfFilms = +prompt("How many films have you already watched?", "");
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  private: false,
+};
+const a = prompt("One of the last movies you watched?", ""),
+  b = prompt("How much would you rate it?", ""),
+  c = prompt("One of the last movies you watched?", ""),
+  d = prompt("How much would you rate it?", "");
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
