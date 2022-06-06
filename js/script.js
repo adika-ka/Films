2c45094f90d8a604eeed56553d63d2a16a8347a2
adika-ka <adika-ka@mail.ru>
2022-06-06 13:21:10 +0300

    funtion

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
-const numberOfFilms = +prompt("How many films have you already watched?", "");
+let numberOfFilms;
+
+function start() {
+  numberOfFilms = +prompt("How many films have you already watched?", "");
+  while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
+    numberOfFilms = +prompt("How many films have you already watched?", "");
+  }
+}
+
+start();
+
 const personalMovieDB = {
   count: numberOfFilms,
   movies: {},
@@ -6,9 +16,48 @@ const personalMovieDB = {
   genres: [],
   private: false,
 };
-const a = prompt("One of the last movies you watched?", ""),
-  b = prompt("How much would you rate it?", ""),
-  c = prompt("One of the last movies you watched?", ""),
-  d = prompt("How much would you rate it?", "");
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+
+function rememberMyFilms() {
+  for (let i = 0; i < 2; i++) {
+    const a = prompt("One of the last movies you watched?", ""),
+      b = prompt("How much would you rate it?", "");
+
+    if (a != "" && b != "" && a !== null && b !== "" && a.length < 50) {
+      personalMovieDB.movies[a] = b;
+    } else {
+      i--;
+    }
+  }
+}
+
+rememberMyFilms();
+
+function detectPersonalLevel() {
+  if (personalMovieDB.count < 10) {
+    console.log("Watched quite a few films.");
+  } else if (personalMovieDB.count >= 10 && personalMovieDB < 30) {
+    console.log("You are a great viewer.");
+  } else if (personalMovieDB.count >= 30) {
+    console.log("You are a movie buff.");
+  } else {
+    console.log("Error.");
+  }
+}
+
+detectPersonalLevel();
+
+function showMyDB() {
+  if (!personalMovieDB.private) {
+    console.log(personalMovieDB);
+  }
+}
+
+showMyDB();
+
+function writeYourGenres() {
+  for (let i = 1; i <= 3; i++) {
+    personalMovieDB.genres[i - 1] = prompt(`Your favorite genre is ${i}`);
+  }
+}
+
+writeYourGenres();
